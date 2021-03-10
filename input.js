d908d1a7bc30585db66a425a69df89a4fde643dc
Sam <sribas@gmail.com>
2021-03-10 19:22:33 +0000

    Week2/Day3 - Input Module

---
+++ input.js
@@ -0,0 +1,19 @@
+/**
+ * Setup User Interface 
+ * Specifically, so that we can handle user input via stdin
+ */
+ const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  return stdin;
+};
+
+const handleUserInput = (key) => {
+  if(key === `\u0003`) {
+    process.exit(); 
+  }
+};
+
+module.exports = { setupInput };
--- play.js
+++ play.js
@@ -1,23 +1,8 @@
 const { connect } = require('./client');
+const { setupInput } = require('./input');
 //const move = require('./client');
 
 console.log('Connecting...');
 connect();
 
-/**
- * Setup User Interface 
- * Specifically, so that we can handle user input via stdin
- */
- const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  return stdin;
-};
-
-const handleUserInput = (key) => {
-  if(key === `\u0003`) {
-    process.exit(); 
-  }
-};
+setupInput();
